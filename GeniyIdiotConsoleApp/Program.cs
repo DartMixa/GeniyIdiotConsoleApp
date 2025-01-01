962207e67f61aab0e6103b3364566bb0ca8c5abb
DartMixa <mar20060228@mail.ru>
2025-01-01 15:48:50 +0300

    добавил повтор теста

--- GeniyIdiotConsoleApp/Program.cs
+++ GeniyIdiotConsoleApp/Program.cs
@@ -31,39 +31,49 @@ namespace GeniyIdiotConsoleApp
 
         static void Main(string[] args)
         {
-            int countQuestions = 5;
-            List<Tuple<string, int>> questions = GetQuestionsAndAnswers();
-
-            int countRightAnswers = 0;
-
-            Random random = new Random();
-
             Console.WriteLine("Здравствуйте, как вас зовут?");
             string? userName = Console.ReadLine();
-
-            for (int i = 0; i < countQuestions; i++)
+            while (true)
             {
-                Console.WriteLine("Вопрос №" + (i + 1));
+                int countQuestions = 5;
+                List<Tuple<string, int>> questions = GetQuestionsAndAnswers();
 
-                int randomQuestionIndex = random.Next(0, questions.Count);
-                Console.WriteLine(randomQuestionIndex);
-                Console.WriteLine(questions[randomQuestionIndex].Item1);
+                int countRightAnswers = 0;
 
-                int userAnswer = Convert.ToInt32(Console.ReadLine());
+                Random random = new Random();
 
-                int rightAnswer = questions[randomQuestionIndex].Item2;
 
-                if (userAnswer == rightAnswer)
+
+                for (int i = 0; i < countQuestions; i++)
                 {
-                    countRightAnswers++;
+                    Console.WriteLine("Вопрос №" + (i + 1));
+
+                    int randomQuestionIndex = random.Next(0, questions.Count);
+                    Console.WriteLine(randomQuestionIndex);
+                    Console.WriteLine(questions[randomQuestionIndex].Item1);
+
+                    int userAnswer = Convert.ToInt32(Console.ReadLine());
+
+                    int rightAnswer = questions[randomQuestionIndex].Item2;
+
+                    if (userAnswer == rightAnswer)
+                    {
+                        countRightAnswers++;
+                    }
+                    questions.Remove(questions[randomQuestionIndex]);
                 }
-                questions.Remove(questions[randomQuestionIndex]);
+
+                Console.WriteLine("Количество правильных ответов: " + countRightAnswers);
+
+
+                Console.WriteLine(userName + ", ваш диагноз:" + GetDiagnoses()[countRightAnswers]);
+
+                Console.WriteLine("Хотите пройти тест ещё раз?");
+                if (Console.ReadLine().ToLower() == "нет") 
+                {
+                    break;
+                }
             }
-
-            Console.WriteLine("Количество правильных ответов: " + countRightAnswers);
-
-            
-            Console.WriteLine(userName + ", ваш диагноз:" + GetDiagnoses()[countRightAnswers]);
         }
     }
 }

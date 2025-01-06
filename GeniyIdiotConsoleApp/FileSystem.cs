3c46e7ebb593047ab926efe2041e02876284e59d
DartMixa <mar20060228@mail.ru>
2025-01-06 16:41:03 +0300

    Написал класс для работы с файловой системой

---
+++ GeniyIdiotConsoleApp/FileSystem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeniyIdiotConsoleApp
+{
+    static class FileSystem
+    {
+        static public string ReadFile(string fileName)
+        {
+            var sr = new StreamReader(System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "/" + fileName + ".txt");
+            var txt = sr.ReadToEnd();
+            sr.Close();
+            return txt;
+        }
+        static public void WriteFile(string fileName, string txt)
+        {
+            var sw = new StreamWriter(System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "/" + fileName + ".txt");
+            sw.Write(txt);
+            sw.Close();
+        }
+    }
+}

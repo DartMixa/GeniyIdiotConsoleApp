eeefcb1deeb0273233b7294ccb6de0bc15e3196b
stud <mar20060228@mail.ru>
2025-05-22 10:34:27 +0300

    Начал улучшать добовление и удоление вопросов

--- GeniyIdiotWinFormsApp/AddQuestionForm.Designer.cs
+++ GeniyIdiotWinFormsApp/AddQuestionForm.Designer.cs
@@ -20,108 +20,154 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            questionTextBox = new TextBox();
-            answerTextBox = new TextBox();
-            cancelButton = new Button();
-            acceptButton = new Button();
-            label1 = new Label();
-            label2 = new Label();
-            SuspendLayout();
-            // 
-            // questionTextBox
-            // 
-            questionTextBox.Location = new Point(12, 44);
-            questionTextBox.Multiline = true;
-            questionTextBox.Name = "questionTextBox";
-            questionTextBox.Size = new Size(433, 96);
-            questionTextBox.TabIndex = 0;
-            questionTextBox.TextAlign = HorizontalAlignment.Center;
-            questionTextBox.TextChanged += TextBox_TextChanged;
-            // 
-            // answerTextBox
-            // 
-            answerTextBox.Location = new Point(22, 180);
-            answerTextBox.Name = "answerTextBox";
-            answerTextBox.Size = new Size(413, 23);
-            answerTextBox.TabIndex = 1;
-            answerTextBox.TextAlign = HorizontalAlignment.Center;
-            answerTextBox.TextChanged += TextBox_TextChanged;
-            // 
-            // cancelButton
-            // 
-            cancelButton.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            cancelButton.Location = new Point(37, 221);
-            cancelButton.Name = "cancelButton";
-            cancelButton.Size = new Size(188, 56);
-            cancelButton.TabIndex = 2;
-            cancelButton.Text = "Отмена";
-            cancelButton.UseVisualStyleBackColor = true;
-            cancelButton.Click += cancelButton_Click;
-            // 
-            // acceptButton
-            // 
-            acceptButton.Enabled = false;
-            acceptButton.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            acceptButton.Location = new Point(231, 221);
-            acceptButton.Name = "acceptButton";
-            acceptButton.Size = new Size(188, 56);
-            acceptButton.TabIndex = 3;
-            acceptButton.Text = "Добавить";
-            acceptButton.UseVisualStyleBackColor = true;
-            acceptButton.Click += acceptButton_Click;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            label1.Location = new Point(22, 16);
-            label1.Name = "label1";
-            label1.Size = new Size(150, 25);
-            label1.TabIndex = 4;
-            label1.Text = "Введите вопрос";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            label2.Location = new Point(22, 152);
-            label2.Name = "label2";
-            label2.Size = new Size(203, 25);
-            label2.TabIndex = 5;
-            label2.Text = "Введите ответ (число)";
-            // 
-            // AddQuestionForm
-            // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(457, 289);
-            Controls.Add(label2);
-            Controls.Add(label1);
-            Controls.Add(acceptButton);
-            Controls.Add(cancelButton);
-            Controls.Add(answerTextBox);
-            Controls.Add(questionTextBox);
-            Name = "AddQuestionForm";
-            Text = "Добавить вопрос";
-            ResumeLayout(false);
-            PerformLayout();
-        }
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			questionTextBox = new TextBox();
+			answerTextBox = new TextBox();
+			acceptButton = new Button();
+			label1 = new Label();
+			label2 = new Label();
+			questionsDataGridView = new DataGridView();
+			Column3 = new DataGridViewTextBoxColumn();
+			Column1 = new DataGridViewTextBoxColumn();
+			Column2 = new DataGridViewTextBoxColumn();
+			label3 = new Label();
+			((System.ComponentModel.ISupportInitialize)questionsDataGridView).BeginInit();
+			SuspendLayout();
+			// 
+			// questionTextBox
+			// 
+			questionTextBox.Location = new Point(641, 114);
+			questionTextBox.Multiline = true;
+			questionTextBox.Name = "questionTextBox";
+			questionTextBox.Size = new Size(381, 96);
+			questionTextBox.TabIndex = 0;
+			questionTextBox.TextAlign = HorizontalAlignment.Center;
+			questionTextBox.TextChanged += TextBox_TextChanged;
+			// 
+			// answerTextBox
+			// 
+			answerTextBox.Location = new Point(641, 271);
+			answerTextBox.Name = "answerTextBox";
+			answerTextBox.Size = new Size(381, 23);
+			answerTextBox.TabIndex = 1;
+			answerTextBox.TextAlign = HorizontalAlignment.Center;
+			answerTextBox.TextChanged += TextBox_TextChanged;
+			// 
+			// acceptButton
+			// 
+			acceptButton.Enabled = false;
+			acceptButton.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+			acceptButton.Location = new Point(641, 330);
+			acceptButton.Name = "acceptButton";
+			acceptButton.Size = new Size(381, 56);
+			acceptButton.TabIndex = 3;
+			acceptButton.Text = "Добавить";
+			acceptButton.UseVisualStyleBackColor = true;
+			acceptButton.Click += acceptButton_Click;
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+			label1.Location = new Point(641, 86);
+			label1.Name = "label1";
+			label1.Size = new Size(150, 25);
+			label1.TabIndex = 4;
+			label1.Text = "Введите вопрос";
+			// 
+			// label2
+			// 
+			label2.AutoSize = true;
+			label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+			label2.Location = new Point(641, 243);
+			label2.Name = "label2";
+			label2.Size = new Size(203, 25);
+			label2.TabIndex = 5;
+			label2.Text = "Введите ответ (число)";
+			// 
+			// questionsDataGridView
+			// 
+			questionsDataGridView.AllowUserToAddRows = false;
+			questionsDataGridView.AllowUserToDeleteRows = false;
+			questionsDataGridView.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+			questionsDataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			questionsDataGridView.Columns.AddRange(new DataGridViewColumn[] { Column3, Column1, Column2 });
+			questionsDataGridView.Location = new Point(12, 12);
+			questionsDataGridView.Name = "questionsDataGridView";
+			questionsDataGridView.ReadOnly = true;
+			questionsDataGridView.Size = new Size(623, 374);
+			questionsDataGridView.TabIndex = 6;
+			questionsDataGridView.KeyDown += questionsDataGridView_KeyDown;
+			// 
+			// Column3
+			// 
+			Column3.HeaderText = "№";
+			Column3.Name = "Column3";
+			Column3.ReadOnly = true;
+			Column3.Width = 30;
+			// 
+			// Column1
+			// 
+			Column1.HeaderText = "Вопрос";
+			Column1.Name = "Column1";
+			Column1.ReadOnly = true;
+			Column1.Width = 500;
+			// 
+			// Column2
+			// 
+			Column2.HeaderText = "Ответ";
+			Column2.Name = "Column2";
+			Column2.ReadOnly = true;
+			Column2.Width = 50;
+			// 
+			// label3
+			// 
+			label3.AutoSize = true;
+			label3.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+			label3.Location = new Point(641, 38);
+			label3.Name = "label3";
+			label3.Size = new Size(164, 25);
+			label3.TabIndex = 7;
+			label3.Text = "Добавить вопрос";
+			// 
+			// AddQuestionForm
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(1028, 408);
+			Controls.Add(label3);
+			Controls.Add(questionsDataGridView);
+			Controls.Add(label2);
+			Controls.Add(label1);
+			Controls.Add(acceptButton);
+			Controls.Add(answerTextBox);
+			Controls.Add(questionTextBox);
+			Name = "AddQuestionForm";
+			Text = "Добавить вопрос";
+			Load += QuestionForm_Load;
+			((System.ComponentModel.ISupportInitialize)questionsDataGridView).EndInit();
+			ResumeLayout(false);
+			PerformLayout();
+		}
 
-        #endregion
+		#endregion
 
-        private TextBox questionTextBox;
+		private TextBox questionTextBox;
         private TextBox answerTextBox;
-        private Button cancelButton;
         private Button acceptButton;
         private Label label1;
         private Label label2;
-    }
+		private DataGridView questionsDataGridView;
+		private DataGridViewTextBoxColumn Column3;
+		private DataGridViewTextBoxColumn Column1;
+		private DataGridViewTextBoxColumn Column2;
+		private Label label3;
+	}
 }
--- GeniyIdiotWinFormsApp/AddQuestionForm.cs
+++ GeniyIdiotWinFormsApp/AddQuestionForm.cs
@@ -11,37 +11,64 @@ using System.Windows.Forms;
 
 namespace GeniyIdiotWinFormsApp
 {
-    public partial class AddQuestionForm : Form
-    {
-        QuestionsStorage storage;
-        public AddQuestionForm(QuestionsStorage storage)
-        {
-            InitializeComponent();
-            this.storage = storage;
-        }
+	public partial class AddQuestionForm : Form
+	{
+		QuestionsStorage storage;
+		public AddQuestionForm(QuestionsStorage storage)
+		{
+			InitializeComponent();
+			this.storage = storage;
+		}
 
-        private void cancelButton_Click(object sender, EventArgs e)
-        {
-            Close();
-        }
+		private void cancelButton_Click(object sender, EventArgs e)
+		{
+			Close();
+		}
 
-        private void acceptButton_Click(object sender, EventArgs e)
-        {
-            storage.Add(new Question(questionTextBox.Text, int.Parse(answerTextBox.Text)));
-            storage.Save();
-            Close();
-        }
+		private void acceptButton_Click(object sender, EventArgs e)
+		{
+			storage.Add(new Question(questionTextBox.Text, int.Parse(answerTextBox.Text)));
+			storage.Save();
+			TableUpdate();
 
-        private void TextBox_TextChanged(object sender, EventArgs e)
-        {
-            if (questionTextBox.Text.Contains(";;;") == false && int.TryParse(answerTextBox.Text, out var answer))
-            {
-                acceptButton.Enabled = true;
-            }
-            else
-            {
-                acceptButton.Enabled = false;
-            }
-        }
-    }
+		}
+
+		private void TextBox_TextChanged(object sender, EventArgs e)
+		{
+			if (questionTextBox.Text.Contains(";;;") == false && int.TryParse(answerTextBox.Text, out var answer))
+			{
+				acceptButton.Enabled = true;
+			}
+			else
+			{
+				acceptButton.Enabled = false;
+			}
+		}
+		private void QuestionForm_Load(object sender, EventArgs e)
+		{
+			TableUpdate();
+		}
+		private void TableUpdate()
+		{
+			questionsDataGridView.Rows.Clear();
+			for (int i = 0; i < storage.Questions.Count; i++)
+			{
+				questionsDataGridView.Rows.Add();
+				questionsDataGridView.Rows[i].Cells[0].Value = i;
+				questionsDataGridView.Rows[i].Cells[1].Value = storage.Questions[i].question;
+				questionsDataGridView.Rows[i].Cells[2].Value = storage.Questions[i].answer;
+			}
+		}
+
+		private void questionsDataGridView_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Delete)
+			{
+				storage.Questions.RemoveAt((int)questionsDataGridView.SelectedRows[0].Cells[0].Value);
+				storage.Save();
+				TableUpdate();
+				
+			}
+		}
+	}
 }

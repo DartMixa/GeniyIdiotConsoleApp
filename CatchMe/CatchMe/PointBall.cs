626df21e307479cf6f055396599dc8a4f79b7dd7
DartMixa <mar20060228@mail.ru>
2025-07-02 22:21:18 +0300

    Написал первую версию игры

---
+++ CatchMe/CatchMe/PointBall.cs
@@ -0,0 +1,11 @@
+﻿namespace CatchMe
+{
+    public class PointBall : Ball
+    {
+        public PointBall(MainForm form, int x, int y) : base(form) 
+        {
+            this.x = x - size / 2;
+            this.y = y - size / 2;
+        }
+    }
+}
---
+++ CatchMe/CatchMe/RandomVelBoll.cs
@@ -0,0 +1,13 @@
+﻿namespace CatchMe
+{
+    class RandomVelBoll : RandomSizeAndPointBall 
+    {
+        public RandomVelBoll(MainForm form) : base(form) 
+        {
+            int angle = random.Next(0, 360);
+            double angleRad = angle * 2 * Math.PI / 360;
+            int randomSpeed = random.Next(5, 20);
+            vel = ((int)Math.Round(randomSpeed * Math.Cos(angleRad)), (int)Math.Round(randomSpeed * Math.Sin(angleRad)));
+        }
+    }
+}

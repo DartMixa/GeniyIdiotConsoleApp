1366813450d74a80d2f8f84f2dbaf012254f4663
DartMixa <mar20060228@mail.ru>
2025-07-09 00:54:20 +0300

    Написал Fruit Ninja

---
+++ CatchMe/BallsClassLibrary/FruitBall.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BallsClassLibrary
+{
+    public class FruitBall : AccelerationVelBall
+    {
+        public FruitBall(List<Ball> balls, Form form) : base(balls)
+        {
+            cord = (random.Next(form.ClientSize.Width / 4, form.ClientSize.Width - form.ClientSize.Width / 4), form.ClientSize.Height);
+            Angle = random.Next(180 + 70, 360 - 70);
+            Speed = random.Next(20, 30);
+            radius = random.Next(20, 30);
+            brush = RandomBrush.Next();
+            Acceleration = new(0, 1f);
+        }
+    }
+}
---
+++ CatchMe/FruitNinja/MainForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace FruitNinja
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            UpdateTimer = new System.Windows.Forms.Timer(components);
+            FruitSpawnTimer = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // UpdateTimer
+            // 
+            UpdateTimer.Interval = 20;
+            UpdateTimer.Tick += Update;
+            // 
+            // FruitSpawnTimer
+            // 
+            FruitSpawnTimer.Enabled = true;
+            FruitSpawnTimer.Interval = 1500;
+            FruitSpawnTimer.Tick += FruitSpawnTimer_Tick;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Name = "MainForm";
+            Text = " Fruit Ninja";
+            MouseMove += MainForm_MouseMove;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer UpdateTimer;
+        private System.Windows.Forms.Timer FruitSpawnTimer;
+    }
+}

92b3c810bf4486d325ab395b0ec1adb23e58d89e
DartMixa <mar20060228@mail.ru>
2025-07-08 23:15:18 +0300

    Исправил ошибки

--- CatchMe/BallsClassLibrary/AccelerationVelBall.cs
+++ CatchMe/BallsClassLibrary/AccelerationVelBall.cs
@@ -22,29 +22,4 @@ namespace BallsClassLibrary
             base.Go();
         }
     }
-    public class PointAccelVelBall : AccelerationVelBall
-    {
-        public PointAccelVelBall(List<Ball> balls, int x, int y) : base(balls)
-        {
-            cord.x = x;
-            cord.y = y;
-        }
-    }
-    public class FireworkBall : PointAccelVelBall
-    {
-        private List<Brush> brushes = [Brushes.Thistle, 
-            Brushes.Aqua,
-            Brushes.Aquamarine, 
-            Brushes.BlueViolet, 
-            Brushes.BlanchedAlmond, 
-            Brushes.Blue, 
-            Brushes.BurlyWood,
-            Brushes.Coral,
-            Brushes.DarkCyan
-        ];
-        public FireworkBall(List<Ball> balls, int x, int y) : base(balls, x, y)
-        {
-            brush = brushes[random.Next(0, brushes.Count)];
-        }
-    }
 }
